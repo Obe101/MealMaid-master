983f9d5ca25ecf2dd478424f3110eb84be425537
Obe <o.bdondo@gmail.com>
2020-10-29 13:03:00 -0700

    Created RecipeDb class with AddRecipe method

--- MealMaid1/Controllers/RecipeController.cs
+++ MealMaid1/Controllers/RecipeController.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MealMaid.Models;
+using MealMaid1.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MealMaid.Controllers
 {
     public class RecipeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RecipeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Recipes()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Recipe r)
+        {
+            if (ModelState.IsValid)
+            {
+                await RecipeDb.AddRecipeAsync(_context, r);
+
+                return RedirectToAction("Recipes");
+            }
             return View();
         }
         
